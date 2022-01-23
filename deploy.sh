c004208b6175474c87bc9cbbace033d2fd70384c
slepoveltech <artemslepov@mail.ru>
2022-01-24 00:02:21 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:slepoveltech/semanic-qa.git master:gh-pages
+git push -f git@github.com:SlepovEltech/semantic-qa.git master:gh-pages
 
 cd -
